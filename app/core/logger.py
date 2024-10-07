4708b79f986549dd2bed5fe47492d408b39f58df
Izpodvypodverta <izpodvypodwerta@gmail.com>
2024-10-07 20:58:24 +0300

    fix logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -4,4 +4,4 @@ import sys
 
 logger.remove()  
 logger.add(sys.stdout, format="{time} {level} {message}", level="INFO", colorize=True)
-logger.add("logs/app_{time}.log", rotation="1 day", retention="7 days", level="DEBUG")
+logger.add("logs/app.log", rotation="1 day", retention="7 days", level="DEBUG")
