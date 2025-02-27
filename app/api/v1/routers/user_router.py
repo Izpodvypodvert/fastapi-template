b1af6b75add31f8262d812af701b72fe536435cd
Alex WSL <izpodvypodwerta@gmail.com>
2025-02-27 20:31:08 +0300

    indentation

--- app/api/v1/routers/user_router.py
+++ app/api/v1/routers/user_router.py
@@ -10,21 +10,19 @@ from app.users.schemas import UserRead, UserCreate, UserUpdate, VerifyEmailReque
 router = APIRouter()
 
 router.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
 )
 
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["auth"]
+    tags=["auth"],
 )
 
 router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
-    tags=["users"]
+    tags=["users"],
 )
 
 router.include_router(
@@ -33,8 +31,11 @@ router.include_router(
     tags=["auth"],
 )
 
+
 @router.post("/auth/verify-email", tags=["auth"])
-async def verify_email(request: VerifyEmailRequest, user_manager: UserManager = Depends(get_user_manager)):
+async def verify_email(
+    request: VerifyEmailRequest, user_manager: UserManager = Depends(get_user_manager)
+):
     try:
         user = await user_manager.verify(request.token)
         return {"message": "Email успешно верифицирован"}
@@ -43,14 +44,15 @@ async def verify_email(request: VerifyEmailRequest, user_manager: UserManager = 
     except UserAlreadyVerified:
         raise HTTPException(status_code=400, detail="Пользователь уже верифицирован")
 
+
 @router.post("/auth/request-verification")
 async def request_verification(
-    user: User = Depends(current_active_user),  
-    user_manager: UserManager = Depends(get_user_manager)
+    user: User = Depends(current_active_user),
+    user_manager: UserManager = Depends(get_user_manager),
 ):
     if user.is_verified:
         raise HTTPException(status_code=400, detail="Пользователь уже верифицирован.")
-    
+
     await user_manager.on_after_request_verify(user)
 
     return {"message": "Письмо для верификации отправлено повторно."}
